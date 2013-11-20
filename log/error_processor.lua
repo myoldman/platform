03d3d5d583db25be0d1160c025027ef94c9b9ac2
myoldman <myoldman@163.com>
2013-11-20 16:02:26 +0800

    test

--- log/error_processor.lua
+++ log/error_processor.lua
@@ -32,34 +32,32 @@ function new(self)
 	return setmetatable({}, mt)
 end
 
-function generel_error_process(self, err)
-	-- get error header acording to request uri
-	local args = ngx.req.get_uri_args()
-	local headers = ngx.req.get_headers()
-	ngx.req.read_body()
-	local post_args;
-	if headers["content-type"] ~= nil and string.match(headers["content-type"], "multipart") ~= nil then
-		post_args = {}
-	else 
-		post_args = ngx.req.get_post_args()
-	end
+	function generel_error_process(self, err)
+		-- get error header acording to request uri
+		local args = ngx.req.get_uri_args()
+		local headers = ngx.req.get_headers()
+		ngx.req.read_body()
+		local post_args;
+		if headers["content-type"] ~= nil and string.match(headers["content-type"], "multipart") ~= nil then
+			post_args = {}
+		else 
+			post_args = ngx.req.get_post_args()
+		end
 
-	local resType = args["resType"] or "2"
-	local error_header = ngx.var.uri .. "_" .. res_type_map[resType]
-
-	local len = #err
-	ngx.status = ngx.HTTP_FORBIDDEN
-	if len  == 1 then
-		output_error_message(string.format(error_message_map[error_header][err[1]]))
-	elseif len == 2  then
-		output_error_message(string.format(error_message_map[error_header][err[1]], err[2]))
-	elseif len == 3 then
-		output_error_message(string.format(error_message_map[error_header][err[1]], err[2], err[3]))
-	else
-		print(err)
-		output_error_message("Internal Server Error")
-	end
-	ngx.exit(ngx.status)
+		local error_header = "general"
+		local len = #err
+		ngx.status = ngx.HTTP_FORBIDDEN
+		if len  == 1 then
+			output_error_message(string.format(error_message_map[error_header][err[1]]))
+		elseif len == 2  then
+			output_error_message(string.format(error_message_map[error_header][err[1]], err[2]))
+		elseif len == 3 then
+			output_error_message(string.format(error_message_map[error_header][err[1]], err[2], err[3]))
+		else
+			print(err)
+			output_error_message("Internal Server Error")
+		end
+		ngx.exit(ngx.status)
 end
 
 local class_mt = {
