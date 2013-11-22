2fdc87a3ae9c928f5754fdf1f5edf47c87aa8acf
myoldman <myoldman@163.com>
2013-11-22 10:39:12 +0800

    a

--- public/public_log_filter.lua
+++ public/public_log_filter.lua
@@ -1,6 +1,9 @@
 local cookie_module = require "cookie.cookie_processor"
 local behavior_module = require "behavior.behavior_recorder"
 
+local cookie = cookie_module:new()
+local behavior_recorder = behavior_module:new()
+
 local cookies = cookie.get_cookie()
 local username = cookies["username"] or ngx.var.username
 behavior_recorder.send_user_behavior_log(username)
