0b45994659eb06621cb826f433cfdf897618e171
myoldman <myoldman@163.com>
2014-01-06 15:24:33 +0800

    a

--- user/user_managment_content.lua
+++ user/user_managment_content.lua
@@ -1,5 +1,5 @@
-local service_module = require(ngx.var.uri) then
-if service_module 
+local service_module = require(ngx.var.uri) 
+if service_module ~= nil then
 	service_module.service_process()
 	ngx.exit(ngx.HTTP_OK)
 else
